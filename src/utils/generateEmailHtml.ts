03a88ccc5016b6fc7c7d75b0116aa31e0194630a
Alexandr Sekerin <blazzed21@gmail.com>
2025-04-27 13:03:54 +0300

    fix: change the padding of the letter and remove the highlighting of links

--- src/utils/generateEmailHtml.ts
+++ src/utils/generateEmailHtml.ts
@@ -5,6 +5,12 @@ import { escapeHtml } from "@/utils";
 export const generateEmailHtml = (data: ContactFormData): string => {
 	return `
     <div style="font-family: Arial, sans-serif; line-height: 1.8; color: #ffffff; background:#303a52; padding: 15px; margin: 15px; border-radius: 20px;">
+      <style>
+        a {
+          color: inherit !important;
+          text-decoration: none !important;
+        }
+      </style>
       <p style="color: #fc85ae; font-size: 24px;"><strong>New Message Received</strong></p>
       <p style="color: #9e579d;"><strong style="color: #ffffff;">Sender's name:</strong> ${escapeHtml(data.author)}</p>
       <p style="color: #9e579d;"><strong style="color: #ffffff;">Email:</strong> ${escapeHtml(data.address)}</p>
@@ -12,7 +18,6 @@ export const generateEmailHtml = (data: ContactFormData): string => {
       <p style="color: #fc85ae; font-size: 16px;">
         <strong strong style="color: #fffff;">Message</strong>
       </p>
-      <br />
         <p style="letter-spacing: 1px;">${escapeHtml(data.message.trim()).replace(/\n/g, "<br>")}</p>
       </div>
       <p style="font-size: 0.8em; color: #999; margin-top:10px; margin-left:10px;">
