8d3efcc5ac2c9d83e18249f3b75ab9ec13347c22
Alexandr Sekerin <blazzed21@gmail.com>
2025-10-05 18:26:26 +0300

    fix: change the metadata directory and add an image for `openGraph`

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -26,30 +26,30 @@ export const GetMetadata = ({
 		description: description,
 		icons: {
 			icon: [
-				{ url: "/icons/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-				{ url: "/icons/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+				{ url: "/metadata/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+				{ url: "/metadata/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
 			],
 			apple: [
-				{ url: "/icons/favicon/apple-icon-57x57.png", sizes: "57x57", type: "image/png" },
-				{ url: "/icons/favicon/apple-icon-60x60.png", sizes: "60x60", type: "image/png" },
-				{ url: "/icons/favicon/apple-icon-72x72.png", sizes: "72x72", type: "image/png" },
-				{ url: "/icons/favicon/apple-icon-76x76.png", sizes: "76x76", type: "image/png" },
-				{ url: "/icons/favicon/apple-icon-114x114.png", sizes: "114x114", type: "image/png" },
-				{ url: "/icons/favicon/apple-icon-120x120.png", sizes: "120x120", type: "image/png" },
-				{ url: "/icons/favicon/apple-icon-144x144.png", sizes: "144x144", type: "image/png" },
-				{ url: "/icons/favicon/apple-icon-152x152.png", sizes: "152x152", type: "image/png" },
-				{ url: "/icons/favicon/apple-icon-180x180.png", sizes: "180x180", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-57x57.png", sizes: "57x57", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-60x60.png", sizes: "60x60", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-72x72.png", sizes: "72x72", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-76x76.png", sizes: "76x76", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-114x114.png", sizes: "114x114", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-120x120.png", sizes: "120x120", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-144x144.png", sizes: "144x144", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-152x152.png", sizes: "152x152", type: "image/png" },
+				{ url: "/metadata/favicon/apple-icon-180x180.png", sizes: "180x180", type: "image/png" },
 			],
 			other: [
 				{
 					rel: "icon",
-					url: "/icons/favicon/android-icon-192x192.png",
+					url: "/metadata/favicon/android-icon-192x192.png",
 					sizes: "192x192",
 					type: "image/png",
 				},
 			],
 		},
-		manifest: "/icons/favicon/manifest.json",
+		manifest: "/metadata/favicon/manifest.json",
 		openGraph: {
 			type: "website",
 			url: domain,
@@ -57,7 +57,7 @@ export const GetMetadata = ({
 			description: description,
 			images: [
 				{
-					url: "/img/metadata/metadata.jpg",
+					url: "/metadata/metadata.jpg",
 					width: 1200,
 					height: 630,
 					alt: title,
@@ -68,11 +68,11 @@ export const GetMetadata = ({
 			card: "summary_large_image",
 			title: title,
 			description: description,
-			images: ["/img/metadata/metadata.jpg"],
+			images: ["/metadata/metadata.jpg"],
 		},
 		other: {
 			"msapplication-TileColor": "#ffffff",
-			"msapplication-TileImage": "/icons/favicon/ms-icon-144x144.png",
+			"msapplication-TileImage": "/metadata/favicon/ms-icon-144x144.png",
 		},
 	};
 };
